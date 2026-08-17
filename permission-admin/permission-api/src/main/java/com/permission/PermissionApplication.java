4213c9e301e28f3176b0c41c78e8a568e0f35b58
werabc <154811047+werabc@users.noreply.github.com>
2026-08-17 18:52:40 +0800

    Add permission-admin/permission-api/src/main/java/com/permission/PermissionApplication.java

---
+++ permission-admin/permission-api/src/main/java/com/permission/PermissionApplication.java
@@ -0,0 +1,14 @@
+package com.permission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PermissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionApplication.class, args);
+    }
+}
+
